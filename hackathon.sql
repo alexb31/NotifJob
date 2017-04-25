b4601988f35e2dc0c5aa92ee244be851643d3c9c
alexb31 <123azertyu>
2017-04-25 19:59:12 +0200

    add database

--- hackathon.sql
+++ hackathon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 25 Avril 2017 à 15:04
+-- Généré le :  Mar 25 Avril 2017 à 17:58
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -23,70 +23,96 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `employees`
+-- Structure de la table `candidats`
 --
 
-CREATE TABLE `employees` (
-  `employeeNumber` int(11) NOT NULL,
-  `lastName` varchar(50) NOT NULL,
-  `firstName` varchar(50) NOT NULL,
-  `extension` varchar(10) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `officeCode` varchar(10) NOT NULL,
-  `file_url` varchar(250) CHARACTER SET utf8 NOT NULL,
-  `jobTitle` varchar(50) NOT NULL
+CREATE TABLE `candidats` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `competences` varchar(255) NOT NULL,
+  `etudes` varchar(255) NOT NULL,
+  `experiences` varchar(255) NOT NULL,
+  `cv` varchar(255) NOT NULL,
+  `lien-linkedin` varchar(255) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
+  `age` varchar(100) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Contenu de la table `employees`
+-- Structure de la table `lm`
 --
 
-INSERT INTO `employees` (`employeeNumber`, `lastName`, `firstName`, `extension`, `email`, `officeCode`, `file_url`, `jobTitle`) VALUES
-(1002, 'Murphy', 'Diane', 'x5800', 'dmurphy@classicmodelcars.com', '1', '', 'President'),
-(1056, 'Patterson', 'Mary', 'x4611', 'mpatterso@classicmodelcars.com', '1', '', 'VP Sales'),
-(1076, 'Firrelli', 'Jeff', 'x9273', 'jfirrelli@classicmodelcars.com', '1', '', 'VP Marketing'),
-(1088, 'Patterson', 'William', 'x4871', 'wpatterson@classicmodelcars.com', '6', '', 'Sales Manager (APAC)'),
-(1102, 'Bondur', 'Gerard', 'x5408', 'gbondur@classicmodelcars.com', '4', '7ab4f-parisescalade-formulaire-inscription.pdf', 'Sale Manager (EMEA)'),
-(1143, 'Bow', 'Anthony', 'x5428', 'abow@classicmodelcars.com', '1', '', 'Sales Manager (NA)'),
-(1165, 'Jennings', 'Leslie', 'x3291', 'ljennings@classicmodelcars.com', '1', '', 'Sales Rep'),
-(1166, 'Thompson', 'Leslie', 'x4065', 'lthompson@classicmodelcars.com', '1', '', 'Sales Rep'),
-(1188, 'Firrelli', 'Julie', 'x2173', 'jfirrelli@classicmodelcars.com', '2', 'test-2.pdf', 'Sales Rep'),
-(1216, 'Patterson', 'Steve', 'x4334', 'spatterson@classicmodelcars.com', '2', '', 'Sales Rep'),
-(1286, 'Tseng', 'Foon Yue', 'x2248', 'ftseng@classicmodelcars.com', '3', '', 'Sales Rep'),
-(1323, 'Vanauf', 'George', 'x4102', 'gvanauf@classicmodelcars.com', '3', '', 'Sales Rep'),
-(1337, 'Bondur', 'Loui', 'x6493', 'lbondur@classicmodelcars.com', '4', '', 'Sales Rep'),
-(1370, 'Hernandez', 'Gerard', 'x2028', 'ghernande@classicmodelcars.com', '4', '', 'Sales Rep'),
-(1401, 'Castillo', 'Pamela', 'x2759', 'pcastillo@classicmodelcars.com', '4', '', 'Sales Rep'),
-(1501, 'Bott', 'Larry', 'x2311', 'lbott@classicmodelcars.com', '7', '', 'Sales Rep'),
-(1504, 'Jones', 'Barry', 'x102', 'bjones@classicmodelcars.com', '7', '', 'Sales Rep'),
-(1611, 'Fixter', 'Andy', 'x101', 'afixter@classicmodelcars.com', '6', '', 'Sales Rep'),
-(1612, 'Marsh', 'Peter', 'x102', 'pmarsh@classicmodelcars.com', '6', '', 'Sales Rep'),
-(1619, 'King', 'Tom', 'x103', 'tking@classicmodelcars.com', '6', '', 'Sales Rep'),
-(1621, 'Nishi', 'Mami', 'x101', 'mnishi@classicmodelcars.com', '5', '', 'Sales Rep'),
-(1625, 'Kato', 'Yoshimi', 'x102', 'ykato@classicmodelcars.com', '5', '', 'Sales Rep'),
-(1702, 'Gerard', 'Martin', 'x2312', 'mgerard@classicmodelcars.com', '4', '', 'Sales Rep'),
-(1703, 'Alex', 'B', 'a', 'a@a.fr', 'a', 'a', 'a'),
-(1704, '', '', '', '', '', 'bfe14-parisescalade-formulaire-inscription.pdf', '');
+CREATE TABLE `lm` (
+  `id` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offres`
+--
+
+CREATE TABLE `offres` (
+  `id` int(11) NOT NULL,
+  `intitule` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `lieu` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `date_debut` date NOT NULL,
+  `date_publication` date NOT NULL,
+  `competences` varchar(255) NOT NULL,
+  `experiences` varchar(255) NOT NULL,
+  `lien_offre` varchar(255) NOT NULL,
+  `etudes` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Index pour les tables exportées
 --
 
 --
--- Index pour la table `employees`
+-- Index pour la table `candidats`
 --
-ALTER TABLE `employees`
-  ADD PRIMARY KEY (`employeeNumber`);
+ALTER TABLE `candidats`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `lm`
+--
+ALTER TABLE `lm`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `offres`
+--
+ALTER TABLE `offres`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT pour la table `employees`
+-- AUTO_INCREMENT pour la table `candidats`
 --
-ALTER TABLE `employees`
-  MODIFY `employeeNumber` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1705;
+ALTER TABLE `candidats`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `lm`
+--
+ALTER TABLE `lm`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `offres`
+--
+ALTER TABLE `offres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
