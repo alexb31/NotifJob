dcbd4e268eeb52ad436e28dfc202bcbe2212e7ce
Unknown <fabio.soares@live.fr>
2017-04-27 00:02:49 +0200

    (Front) Home Page - Integration Statique

---
+++ application/views/_menu.php
@@ -0,0 +1,36 @@
+ <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <img src="../web/img/logo-menu.png" class="img-responsive" alt="">    
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul id="menu" class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#la-une">A la une</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#rechercher-offres">Rechercher des offres</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#mes-offres">Mes offres</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#connexion">Connexion</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
