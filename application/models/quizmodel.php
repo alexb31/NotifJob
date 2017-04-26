27c56dc67f82db982af3b22b1e27b28f217898cc
alexb31 <alexandrebornstein@gmail.com>
2017-04-26 17:49:32 +0200

    Ajout Concours

---
+++ application/models/quizmodel.php
@@ -0,0 +1,21 @@
+<?php
+
+class quizmodel extends CI_Model {
+
+    public function getQuestions()
+    {
+        $this->db->select("id, question, choice1, choice2, choice3, answer");
+        $this->db->from("quiz");
+
+        $query = $this->db->get();
+
+        return $query->result();
+
+        $num_data_returned = $query->num_rows;
+
+        if ($num_data_returned < 1) {
+            echo "Pas de données dans la BDD";
+            exit();
+        }
+    }
+}
