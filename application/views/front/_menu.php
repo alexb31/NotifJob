30443f5daaee00d157faed9c994ddc8bbffa7392
fxbrazier <fx.brazier@gmail.com>
2017-04-28 13:42:16 +0200

    modif lien

--- application/views/front/_menu.php
+++ application/views/front/_menu.php
@@ -5,7 +5,7 @@
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand page-scroll" href="#page-top">
+                <a class="navbar-brand page-scroll" href="<?php echo base_url();?>main">
                     <img src="<?php echo base_url();?>assets/web/img/logo-menu.png" class="img-responsive" alt="">    
                 </a>
             </div>
