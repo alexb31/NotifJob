da24a6ca1e5b63b6a4b39644a1461e06b1f9802c
fxbrazier <fx.brazier@gmail.com>
2017-04-28 12:47:08 +0200

    liaison bdd offre

--- application/controllers/Offre.php
+++ application/controllers/Offre.php
@@ -5,6 +5,16 @@ class Offre extends CI_Controller {
 	
 	public function index()
 	{
-		$this->load->view('front/offre');
+		$id = $this->input->get('id');
+		$id = intval($id);
+
+		$query = $this->db->get_where('offres', array('id' => $id));
+
+		foreach ($query->result_array() as $row){
+  			$data['offre'][] = $row;
+		}
+
+		//var_dump($data['offre']);die;
+		$this->load->view('front/offre', $data);
 	}
 }
